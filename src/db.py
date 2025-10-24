ac047f40e7c30ba1742cf4cbf4e102c326715399
Primika <156254902+primika27@users.noreply.github.com>
2025-10-24 17:50:28 -0400

    Initial commit

---
+++ src/db.py
@@ -0,0 +1,27 @@
+"""
+Database connections
+SPDX - License - Identifier: LGPL - 3.0 - or -later
+Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
+"""
+
+import mysql.connector
+import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+def get_mysql_conn():
+    """Get a MySQL connection using env variables"""
+    return mysql.connector.connect(
+        host=config.DB_HOST,
+        port=config.DB_PORT,
+        user=config.DB_USER,
+        password=config.DB_PASS,
+        database=config.DB_NAME
+    )
+
+def get_sqlalchemy_session():
+    """Get an SQLAlchemy ORM session using env variables"""
+    connection_string = f'mysql+mysqlconnector://{config.DB_USER}:{config.DB_PASS}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}'
+    engine = create_engine(connection_string)
+    Session = sessionmaker(bind=engine)
+    return Session()
